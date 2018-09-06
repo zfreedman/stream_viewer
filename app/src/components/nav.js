453d577e0a2361921f4f89923805357b2f60292b
Zach Freedman <zfreedman92@gmail.com>
2018-09-05 22:39:37 -0400

    feat: Successfully fetching user auth'd id_token on front end after google signin

--- app/src/components/nav.js
+++ app/src/components/nav.js
@@ -1,11 +1,31 @@
 import React from "react";
 
+const googleSignInClass = "g-signin2";
+
 class Nav extends React.Component {
   render () {
     return (
-      <div class="g-signin2" data-onsuccess="onSignIn"></div>
+      <div className="nav">
+        <div className="g-signin2" />
+      </div>
     );
   }
+
+  componentDidMount () {
+    // this is terrible practice, but can't seem to figure out how to hook into
+    // the onsuccess for the sign in button
+    window.responseGoogle = this.responseGoogle;
+
+    document
+      .querySelector(`.${googleSignInClass}`)
+      .setAttribute("data-onsuccess", "responseGoogle");
+  }
+
+  responseGoogle = googleUser => {
+    // console.log(googleUser);
+    const id_token = googleUser.getAuthResponse().id_token;
+    // console.log(id_token);
+  };
 }
 
 export default Nav;
